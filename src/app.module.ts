2a1ec72367a65ea5525d35bc1c7a7b71e9a862c8
shangwfa <ruomu.shang@tuya.com>
2023-07-03 11:49:00 +0800

    feat:接入prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { LoginModule } from './modules/login/login.module';
 import { OpenApiModule } from './modules/open-api/open-api.module';
 import { RedisModule } from './modules/redis/redis.module';
+import { PrismaService } from './services /prisma.service';
 
 @Module({
   imports: [LoginModule, OpenApiModule, RedisModule],
   controllers: [AppController],
-  providers: [],
+  providers: [PrismaService],
 })
 export class AppModule {}
